974ad0a358ec27255a543fb379ccefe458de4685
Donny <69885780+Spark631@users.noreply.github.com>
2023-06-09 17:43:07 +0000

    Added wrong answers

--- check.js
+++ check.js
@@ -3,11 +3,11 @@
 // const answerList = answer.split('');
 // const answerLength = answerList.length;
 // let total = 0;
-// let addOn = 0; // If the letter in the answer is not the same in teachername, 
+// let addOn = 0; // If the letter in the answer is not the same in teachername,
 //                // addon will be subtracted from the teacher name to compensate
 
 // for (let i = 0; i < answerLength; i++) {
-//     if (answerList[i] === teacherName[i - addOn]) { 
+//     if (answerList[i] === teacherName[i - addOn]) {
 //         total++;
 //     } else {
 //         addOn++;
@@ -21,46 +21,49 @@
 // }
 
 function testAnswer(answer, teacherName) {
+  answer = answer.toLowerCase();
+  teacherName = teacherName.toLowerCase();
 
-    answer = answer.toLowerCase();
-    teacherName = teacherName.toLowerCase();
+  const answerList = answer.split("");
+  const answerLength = answerList.length;
+  const teacherNameLength = teacherName.length;
 
-    const answerList = answer.split('');
-    const answerLength = answerList.length;
-    const teacherNameLength = teacherName.length;
-    
-    let total = 0;
-    let addOn = 0; // If the letter in the answer is not the same in teachername, 
-                   // addon will be subtracted from the teacher name to compensate
-    
-    for (let i = 0; i < answerLength; i++) {
-        if (answerList[i] === teacherName[i - addOn]) { 
-            total++;
-        } else {
-            addOn++;
-        }
-    }
-    
-    if ((100/answerLength) * total >= 75 && answerLength <= teacherNameLength + 2 && answerLength >= teacherNameLength - 2) {
-        console.log("You passed with!" + (100/answerLength) * total + "%");
-        console.log("Answer Length: " + answerLength);
-        console.log("Teacher Name Length: " + teacherNameLength);
+  let total = 0;
+  let addOn = 0; // If the letter in the answer is not the same in teachername,
+  // addon will be subtracted from the teacher name to compensate
+
+  for (let i = 0; i < answerLength; i++) {
+    if (answerList[i] === teacherName[i - addOn]) {
+      total++;
     } else {
-        console.log("Prompt");
+      addOn++;
     }
+  }
+
+  if (
+    (100 / answerLength) * total >= 75 &&
+    answerLength <= teacherNameLength + 2 &&
+    answerLength >= teacherNameLength - 2
+  ) {
+
+    console.log("You passed with!" + (100 / answerLength) * total + "%");
+    console.log("Answer Length: " + answerLength);
+    console.log("Teacher Name Length: " + teacherNameLength);
+  } else if (
+    (100 / answerLength) * total > 50 &&
+    (100 / answerLength) * total < 75 &&
+    answerLength <= teacherNameLength + 2 &&
+    answerLength >= teacherNameLength - 2
+  ) {
+
+    console.log("Prompt");
+  } else {
+
+    console.log("You failed with!" + (100 / answerLength) * total + "%");
+    console.log("Answer Length: " + answerLength);
+    console.log("Teacher Name Length: " + teacherNameLength);
+  }
 }
 
+testAnswer("Kronled", "Kronledge");
 
-testAnswer("MMadoos", "Madoo");
-/*
-run case:
-
-Madoo : 100%
-M : Prompt
-Mad : 100%
-Madooss : Prompt
-Massdoo : prompt
-MMadoo : 83.33333333333334%
-MMadoos : prompt
-
-*/
