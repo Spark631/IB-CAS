ff809fe2f420602d8ba1e0e599a5af28d898b3b2
Donny Nguyen <donnynguyen631@gmail.com>
2023-06-14 01:17:16 -0400

    Potential

--- server.js
+++ server.js
@@ -7,26 +7,32 @@ const io = new Server(server);
 
 var port = Number(process.env.port) || 1337;
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
-
-
 
 io.on("connection", (socket) => {
-    socket.on("join lobby", function(roomid) {
-        socket.join(roomid)
-        console.log("we have joined the room?")
-        console.log("This is the roomid" + roomid)
-    });
-});
-  
-  
+  socket.on("join lobby", function (roomid) {
+    socket.join(roomid);
+    console.log("we have joined the room?");
+    console.log("This is the roomid" + roomid);
 
+    //send to client
 
-
-server.listen(port, () => {
-    console.log("listening on *:" + port);
+    const rooms = io.sockets.adapter.rooms;
+    if (rooms.has(roomid) && rooms.get(roomid).has(socket.id)) {
+      console.log("User is in the room: " + roomid);
+    } else {
+      console.log("User is not in the room: " + roomid);
+    }
   });
 
-  
+  socket.on("check room", function (roomid) {
+    console.log("checking room");
+    console.log(roomid);
+  });
+});
+
+server.listen(port, () => {
+  console.log("listening on *:" + port);
+});
