8679fb6450076da28e11ae1e5124b333d8502473
Donny Nguyen <donnynguyen631@gmail.com>
2023-06-17 15:07:09 -0400

    Create, Join, and text chat

--- server.js
+++ server.js
@@ -7,29 +7,81 @@ const io = new Server(server);
 
 var port = Number(process.env.port) || 1337;
 
+let rooms = [
+  { roomid: "1234", users: [] },
+  { roomid: "5678", users: [] },
+];
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/room.html", (req, res) => {
+  res.sendFile(__dirname + "/room.html");
+});
+
 io.on("connection", (socket) => {
   socket.on("join lobby", function (roomid) {
+    let lobbyCheck = false;
+    for (let i = 0; i < rooms.length; i++) {
+      if (rooms[i].roomid == roomid) {
+        rooms[i].users.push(socket.id);
+        console.log("User - " + socket.id + " has joined the room: " + roomid);
+        lobbyCheck = true;
+        break;
+      }
+    }
+
+    if (lobbyCheck == false) {
+      console.log("Room does not exist");
+      return;
+    }
+
     socket.join(roomid);
-    console.log("we have joined the room?");
     console.log("This is the roomid" + roomid);
 
-    //send to client
+    // const rooms = io.sockets.adapter.rooms;
+    // if (rooms.has(roomid) && rooms.get(roomid).has(socket.id)) {
+    //   console.log("User is in the room: " + roomid);
+    // } else {
+    //   console.log("User is not in the room: " + roomid);
+    // }
+  });
 
-    const rooms = io.sockets.adapter.rooms;
-    if (rooms.has(roomid) && rooms.get(roomid).has(socket.id)) {
-      console.log("User is in the room: " + roomid);
-    } else {
-      console.log("User is not in the room: " + roomid);
+  socket.on("chat message", (msg, clientID) => {
+    for (let i = 0; i < rooms.length; i++) {
+      if (rooms[i].users.includes(clientID)) {
+        io.to(rooms[i].roomid).emit("chat message", msg);
+      }
     }
   });
 
   socket.on("check room", function (roomid) {
     console.log("checking room");
     console.log(roomid);
+  });
+
+  socket.on("create room", function (roomid) {
+    let temp = { roomid: roomid, users: [socket.id] };
+    rooms.push(temp);
+    socket.join(roomid);
+    socket.emit("room created", roomid);
+  });
+
+  socket.on("bye", (arg) => {
+    console.log(arg + " has meowmeowmoewmoew");
+  });
+
+  socket.on("disconnect", () => {
+    for (let i = 0; i < rooms.length; i++) {
+      if (rooms[i].users.includes(socket.id)) {
+        rooms[i].users.splice(rooms[i].users.indexOf(socket.id), 1);
+        console.log(
+          "User - " + socket.id + " has left the room: " + rooms[i].roomid
+        );
+        break;
+      }
+    }
   });
 });
 
