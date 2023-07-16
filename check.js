8b575635623f04cf7410601aaa70e7db1095960e
Derek <83658956+das150@users.noreply.github.com>
2023-07-15 20:59:09 -0400

    Two correct answers
    Now works if there are two correct answers... I think

--- check.js
+++ check.js
@@ -1,10 +1,12 @@
-function testAnswer(answer, teacherName) {
+function testAnswer(answer, teacherName1, teacherName2) {
   answer = answer.toLowerCase();
-  teacherName = teacherName.toLowerCase();
+  teacherName1 = teacherName1.toLowerCase();
+  teacherName2 = teacherName2.toLowerCase();
 
   const answerList = answer.split("");
   const answerLength = answerList.length;
-  const teacherNameLength = teacherName.length;
+  const teacherNameLength1 = teacherName1.length;
+  const teacherNameLength2 = teacherName2.length;
 
   let total = 0;
   let addOn = 0; // If the letter in the answer is not the same in teachername,
@@ -14,16 +16,12 @@ function testAnswer(answer, teacherName) {
   let totalTwo = 0;
 
   for (let i = 0; i < answerLength; i++) {
-    if (answerList[i] == teacherName[i - addOn]) {
+    if (answerList[i] == teacherName1[i - addOn]) {
       totalOne++;
+    } else if (answerList[i] == teacherName2[i - addOn]) {
+      totalTwo++;
     } else {
       addOn++;
-    }
-  }
-
-  for (let j = 0; j < answerLength; j++) {
-    if (answerList[j] == teacherName[j]) {
-      totalTwo++;
     }
   }
 
@@ -35,24 +33,30 @@ function testAnswer(answer, teacherName) {
 
   if (
     (100 / answerLength) * total >= 75 &&
-    answerLength <= teacherNameLength + 2 &&
-    answerLength >= teacherNameLength - 2
+    (
+      (answerLength <= teacherNameLength1 + 2 && answerLength >= teacherNameLength1 - 2) ||
+      (answerLength <= teacherNameLength2 + 2 && answerLength >= teacherNameLength2 - 2)
+    )
   ) {
-    console.log("You passed with!" + (100 / answerLength) * total + "%");
+    console.log("You passed with " + (100 / answerLength) * total + "%");
     console.log("Answer Length: " + answerLength);
-    console.log("Teacher Name Length: " + teacherNameLength);
+    console.log("Teacher Name 1 Length: " + teacherNameLength1);
+    console.log("Teacher Name 2 Length: " + teacherNameLength2);
   } else if (
     (100 / answerLength) * total > 50 &&
     (100 / answerLength) * total < 75 &&
-    answerLength <= teacherNameLength + 2 &&
-    answerLength >= teacherNameLength - 2
+    (
+      (answerLength <= teacherNameLength1 + 2 && answerLength >= teacherNameLength1 - 2) ||
+      (answerLength <= teacherNameLength2 + 2 && answerLength >= teacherNameLength2 - 2)
+    )
   ) {
     console.log("Prompt");
   } else {
-    console.log("You failed with!" + (100 / answerLength) * total + "%");
+    console.log("You failed with " + (100 / answerLength) * total + "%");
     console.log("Answer Length: " + answerLength);
-    console.log("Teacher Name Length: " + teacherNameLength);
+    console.log("Teacher Name 1 Length: " + teacherNameLength1);
+    console.log("Teacher Name 2 Length: " + teacherNameLength2);
   }
 }
 
-testAnswer("kronledge", "Kronledge");
+testAnswer("two", "2", "Two");
