1bd0b1151548f3262dfcd886e05f472a8939cc5d
das150 <83658956+das150@users.noreply.github.com>
2023-07-18 00:44:47 -0400

    Started

---
+++ check.js
@@ -0,0 +1,69 @@
+function testAnswer(answer, ...teacherNames) {
+    answer = answer.toLowerCase();
+    teacherNames = teacherNames.map(name => name.toLowerCase());
+  
+    const answerList = answer.split("");
+    const answerLength = answerList.length;
+    const teacherNameLengths = teacherNames.map(name => name.length);
+  
+    const results = [];
+  
+    for (let i = 0; i < teacherNames.length; i++) {
+      let total = 0;
+      let addOn = 0;
+      let matchCount = 0;
+  
+      for (let j = 0; j < answerLength; j++) {
+        if (answerList[j] == teacherNames[i][j - addOn]) {
+          total++;
+          matchCount++;
+        } else {
+          addOn++;
+        }
+      }
+  
+      const percentage = (100 / answerLength) * total;
+  
+      if (
+        percentage >= 75 &&
+        (answerLength <= teacherNameLengths[i] + 2 && answerLength >= teacherNameLengths[i] - 2)
+      ) {
+        results.push({
+          teacherName: teacherNames[i],
+          passed: true,
+          percentage: percentage,
+          matchCount: matchCount
+        });
+      } else if (percentage > 50 && percentage < 75 &&
+        (answerLength <= teacherNameLengths[i] + 2 && answerLength >= teacherNameLengths[i] - 2)
+      ) {
+        results.push({
+          teacherName: teacherNames[i],
+          passed: false,
+          prompt: true,
+          percentage: percentage,
+          matchCount: matchCount
+        });
+      } else {
+        results.push({
+          teacherName: teacherNames[i],
+          passed: false,
+          percentage: percentage,
+          matchCount: matchCount
+        });
+      }
+    }
+  
+    for (let i = 0; i < results.length; i++) {
+      const result = results[i];
+      if (result.passed) {
+        return "pass";
+      } else if (result.prompt) {
+        return "prompt";
+      } else {
+        return "fail";
+      }
+    }
+  }
+
+  module.exports = { testAnswer };
--- server.js
+++ server.js
@@ -5,36 +5,90 @@ const socketIO = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
+const { testAnswer } = require('./check');
+const users = new Map();
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-  });
-  
+  res.sendFile(__dirname + '/home.html');
+});
+
 // Set up event handler for new socket connections
 io.on('connection', (socket) => {
-  console.log('A user connected');
+
+  let roomId;
+  
+  let playerId = socket.handshake.query.playerId;
+   // Set player Id
+   socket.on('setPlayerId', (newPlayerId) => {
+        playerId = newPlayerId;
+        console.log('User id set to ' + playerId);
+  });
+
+  console.log('User connected');
 
   // Join a room
   socket.on('joinRoom', (room) => {
     socket.join(room);
-    console.log(`User joined room: ${room}`);
+    users.set(socket.id, { playerId, room, score: 0 });
+    console.log('User with id ' + playerId + ' joined room ' + room);
+    socket.to(room).emit('userJoined', playerId);
+    roomId = room;
   });
+
+  // Get users in a room
+  socket.on('getUsersInRoom', (room) => {
+    const usersInRoom = Array.from(users.values())
+      .filter(user => user.room === room)
+      .map(user => ({ username: user.playerId, score: user.score }));
+    console.log(`Users in room ${room}:`, usersInRoom);
+    socket.emit('usersInRoom', usersInRoom);
+  });
+
+// Update user score
+socket.on('updateScore', (score) => {
+  const user = Array.from(users.values()).find(user => user.playerId === playerId);
+  if (user) {
+    user.score = score;
+    console.log(`Updated score for ${playerId} in room ${user.room}: ${score}`);
+  }
+});
 
   // Leave a room
   socket.on('leaveRoom', (room) => {
+    socket.to(room).emit('userLeft', playerId);
     socket.leave(room);
     console.log(`User left room: ${room}`);
+    users.delete(socket.id);
   });
 
   // Handle chat messages
   socket.on('chatMessage', (room, message) => {
+    var answerGrade = testAnswer(message, "Derek Steriods", "Donny Kronladge", "Noah Pielmier");
+    if (answerGrade === "pass") {
+      console.log("Passed!");
+      const user = Array.from(users.values()).find(user => user.playerId === playerId);
+  if (user) {
+    user.score+=10;
+    console.log(`Updated score for ${playerId} in room ${user.room}: ${user.score}`);
+  }
+    } else if (answerGrade === "prompt") {
+        console.log("Prompt!");
+    } else if (answerGrade === "fail") {
+        console.log("Failed!");
+    }
+
     // Broadcast the message to all sockets in the room
     socket.to(room).emit('chatMessage', message);
   });
 
   // Handle disconnections
   socket.on('disconnect', () => {
-    console.log('A user disconnected');
+    console.log('A user with id ' + playerId + ' disconnected');
+    if (roomId) {
+      const { playerId } = users.get(socket.id);
+      io.to(roomId).emit('userLeft', playerId);
+      users.delete(socket.id);
+    }
   });
 });
 
